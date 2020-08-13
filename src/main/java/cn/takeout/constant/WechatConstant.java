8054d4ecf9e17ffc67e1bb38a5eea4c9fae78245
iAmHZiY <793481027@qq.com>
2020-08-13 12:50:48 +0800

    WechatConstant.java

--- src/main/java/cn/takeout/constant/WechatConstant.java
+++ src/main/java/cn/takeout/constant/WechatConstant.java
@@ -6,4 +6,7 @@ public class WechatConstant {
     public final static String APPID = "wx732fcdde42fd6ea8";
     public final static String SECRET = "b98c76bfbd0f72e40cb3aa7d32c7972f";
 
+    public static void main(String[] args) {
+
+    }
 }
